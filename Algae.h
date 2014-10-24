7531c189d2ea5b4597d9872155396042d23b8d48
aerialrave <djduplicitousintent@gmail.com>
2014-10-24 10:09:58 -0400

    Create Algae.h

---
+++ Algae.h
@@ -0,0 +1,21 @@
+#ifndef ALGAE_H
+#define ALGAE_H
+
+#include "Point3D.h"
+class ALGAE
+{
+// holds value for locations 
+private:
+	     Point3d
+public:
+		ALGAE(){}; // constructor
+		// Get and set can be overidden by whatever I need the Iterator that will parse the particles for 
+	Setlocation(double X,double Y,double Z)
+		{
+		}
+		Getlocation()
+		{
+		}
+		
+	};
+	#endif
