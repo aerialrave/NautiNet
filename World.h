8a0ff10f6469dcf2130fc44adbb0babb5c69db82
aerialrave <djduplicitousintent@gmail.com>
2014-10-24 10:10:32 -0400

    Create World.h

---
+++ World.h
@@ -0,0 +1,27 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "Point3D.h" 
+class World
+{
+// holds value for locations 
+private:
+	     Point3D//point 3d object
+		 
+		 
+public:
+		World(){}; // constructor
+	NNWorld
+	Algae
+	
+	update
+	
+	Setlocation(double X,double Y,double Z)
+		{
+		}
+		Getlocation()
+		{
+		}
+		
+	};
+	#endif
